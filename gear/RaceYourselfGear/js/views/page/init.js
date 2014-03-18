7414732259f27b515ad7f037bf3f37ac020c706b
Janne Husberg <janne@husberg.fi>
2014-03-18 09:52:41 +0000

    Fling bugfix.

--- gear/RaceYourselfGear/js/views/page/init.js
+++ gear/RaceYourselfGear/js/views/page/init.js
@@ -36,8 +36,8 @@ define({
         }
 
         function onTouchEnd(ev) {        
-            var dx = ev.changedTouches.item(0).pageX - touch_start.screenX;
-        	var dy = ev.changedTouches.item(0).pageY - touch_start.screenY;
+            var dx = ev.changedTouches.item(0).pageX - touch_start.pageX;
+        	var dy = ev.changedTouches.item(0).pageY - touch_start.pageY;
         	if (dx >= fling_limit.x) {
         	    e.fire('fling.right');
         	}
