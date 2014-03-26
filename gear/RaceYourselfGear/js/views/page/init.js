17933145a9e3a6028e5e0552f615862884c23848
Janne Husberg <janne@husberg.fi>
2014-03-26 09:30:34 +0000

    New error screen. Tizen.back bugfix

--- gear/RaceYourselfGear/js/views/page/init.js
+++ gear/RaceYourselfGear/js/views/page/init.js
@@ -69,10 +69,11 @@ define({
             var html = '<p><b>' + msg + '</b></p>' + url.substr(url.lastIndexOf('/')+1) + ':' + line;
             if (error !== undefined && error.stack !== undefined) html = '<p>' + error.stack + '</p>';
             document.getElementById('error-message').innerHTML = html;
+            window.addEventListener('pageshow', function(ev) { ev.stopPropagation(); ev.preventDefault(); }, true);
             window.addEventListener('pagehide', function(ev) { ev.stopPropagation(); ev.preventDefault(); }, true);
             gear.ui.changePage('#error-page');
             document.getElementById('error-page').addEventListener('click', function() {
-                app.closeApplication();                
+            	app.closeApplication();
             });
         }
 
